c66a333193a780662ef775f09b763c2ca21b784a
puszolek <kochanska.paula@gmail.com>
2016-10-05 14:00:14 +0200

    decoding

--- holo_compr.py
+++ holo_compr.py
@@ -4,41 +4,129 @@ import cv2
 import numpy as np
 import Image
 import os
+import math
+import io
+
+path = os.getcwd() + "\\Test\\"
+
+def show_image(name, image):
+	
+	cv2.imshow(name, image)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
 
 def extract_bitplanes(image):
 
-	path = os.getcwd() + "\\Test\\"
 	if not os.path.exists(path):
 		os.makedirs(path)
 
 	bit_planes = 8
 	for i in range(0,bit_planes):
 		bit_plane = get_bit_plane(image, 0b00000001 << i)
-		bit_plane.save(path + "{}.bmp".format(i))
+		cv2.imwrite(path + "{}.bmp".format(i), bit_plane)
+		print "Image {}.bmp has been successfully written to {}.".format(i,path)
 
 
 def get_bit_plane(mat, mask):
 
-	rows, cols, channels = mat.shape
+	rows, cols = mat.shape
 	bitplane = np.zeros(mat.shape, dtype=np.uint8)
 
 	for i in range(0, rows):
 		for j in range(0, cols):
-			px = mat.item(i,j,0) & mask
-			bitplane.itemset((i,j,0),px)
+			px = mat.item(i,j) & mask
+			bitplane.itemset((i,j),px)
 			
-	bp = Image.fromarray(bitplane)
-	return bp
+	return bitplane
+
+
+def encode_RLE(file):
+
+	mat = cv2.imread(path + file, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+	rows, cols = mat.shape
+	RLE = []
+
+	for i in range(0, rows):
+		row = []
+		if (mat.item(i,0) != 0):
+			row.append(np.uint8(1))
+		elif (mat.item(i,0) == 0):
+			row.append(np.uint8(0))
+
+		j = 0
+		while j < cols:
+			length = 1
+			while ((j+1 < cols) and (mat.item(i,j) == mat.item(i,j+1))):
+				length = length + 1
+				j += 1
+			row.append(np.uint8(length))
+			j += 1
+
+		RLE.append(row)
+	return RLE
+
+
+def decode_RLE(data, mask):
+	
+	rows = len(data)
+	cols = np.sum(data[0][1:])
+	
+	decoded_image = np.zeros((rows, cols, 1), np.uint8)
+
+	for i in range(0, rows):
+		tmp = 0
+		if data[i][0] == 0:
+			color = 1
+		else:
+			color = 0
+
+		for j in range(0, len(data[i])):
+			color = color % 2
+			cv2.line(decoded_image, (tmp,i), (tmp+data[i][j],i), (color*mask,color*mask,color*mask), 1)
+			tmp += data[i][j]
+			color += 1
+
+	return decoded_image
+
+
+def join_images(images):
+	
+	rows = len(images[0])
+	cols = len(images[0][0])
+
+	final_image = np.zeros((rows, cols, 1), np.uint8)
+
+	for img in images:
+		for row in range(0, len(final_image)):
+			for col in range(0, len(final_image[0])):
+				final_image[row][col] += img[row][col]
+
+	return final_image
+
 
 def main():
 
 	file_name = "test_2000mm.bmp"
-	image = cv2.imread(file_name)
-	cv2.imshow('image', image)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-	extract_bitplanes(image)
+	image = cv2.imread(file_name, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+	show_image(file_name, image)
+	#extract_bitplanes(image)
 
+	files = os.listdir(path)
+	decoded_images = []
+	counter = 0
+
+	for file in files:
+		RLE = encode_RLE(file)
+		mask = 0b00000001 << counter
+		decoded_image = decode_RLE(RLE, mask)
+		decoded_images.append(decoded_image)
+		counter += 1
+		
+		#show_image(str(file), decoded_image)
+		
+	final_image = join_images(decoded_images)
+	show_image('final', final_image)
 
 
 main()
